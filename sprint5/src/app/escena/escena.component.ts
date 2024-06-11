809edbccc34098b59106160d826dbf9cb8e1c405
claudiaamprimo <claudia.amprimo@gmail.com>
2024-06-11 21:42:26 +0200

    ejercicio 6 nivel 3

--- sprint5/src/app/escena/escena.component.ts
+++ sprint5/src/app/escena/escena.component.ts
@@ -13,16 +13,25 @@ import { CommonModule } from '@angular/common';
 export class EscenaComponent {
   @Input() steps: IStep[] = [];
   currentStep: number = 0;
+  animationClass: string = '';
 
   nextStep() {
     if (this.currentStep < this.steps.length - 1) {
+      this.animationClass = 'slide-right';
+      setTimeout(() => {
         this.currentStep++;
+        this.animationClass = '';
+      }, 500);
     }
   }
 
   prevStep() {
     if (this.currentStep > 0) {
+      this.animationClass = 'slide-left';
+      setTimeout(() => {
         this.currentStep--;
+        this.animationClass = '';
+      }, 500);
     }
   }
 
@@ -35,6 +44,14 @@ export class EscenaComponent {
   }
 
   handleDots(index: number) {
+    if (index > this.currentStep) {
+      this.animationClass = 'slide-right';
+    } else {
+      this.animationClass = 'slide-left';
+    }
+    setTimeout(() => {
       this.currentStep = index;
+      this.animationClass = '';
+    }, 500);
   }
 }
